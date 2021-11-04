3b00a676965b39d255433d951733efd56def45b5
plussoftjo <44112848+plussoftjo@users.noreply.github.com>
2021-11-04 20:02:05 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,21 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello from windows !</Text>
+      <View
+        style={{
+          height: 500,
+          width: "100%",
+          backgroundColor: "yellow",
+          flexDirection: "row",
+        }}
+      >
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+          <Text style={{ color: "red" }}>Hello </Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "red" }}>
+          <Text style={{ color: "white" }}>World</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
